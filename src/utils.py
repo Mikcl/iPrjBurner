1b12bdac58194160c8b3a9d740dbacb73d6918ac
Mikcl <mikesmikes400@gmail.com>
2020-10-22 20:12:36 +0100

    refactor

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,8 @@ import numpy as np
 import matplotlib.pylab as pl
 import glob
 
+import torch
+
 TARGET_SIZE = 40
 
 def np2pil(a):
@@ -69,3 +71,62 @@ def load_emoji(emoji):
   code = hex(ord(emoji))[2:].lower()
   url = 'https://github.com/googlefonts/noto-emoji/raw/master/png/128/emoji_u%s.png'%code
   return load_image(url)
+
+#@title Train Utilities (SamplePool, Damage)
+class SamplePool:
+  def __init__(self, *, _parent=None, _parent_idx=None, **slots):
+    self._parent = _parent
+    self._parent_idx = _parent_idx
+    self._slot_names = slots.keys()
+    self._size = None
+    for k, v in slots.items():
+      if self._size is None:
+        self._size = len(v)
+      assert self._size == len(v)
+      setattr(self, k, np.asarray(v))
+
+  def sample(self, n):
+    idx = np.random.choice(self._size, n, False)
+    batch = {k: getattr(self, k)[idx] for k in self._slot_names}
+    batch = SamplePool(**batch, _parent=self, _parent_idx=idx)
+    return batch
+
+  def commit(self):
+    for k in self._slot_names:
+      getattr(self._parent, k)[self._parent_idx] = getattr(self, k)
+
+def to_alpha(x):
+  return torch.clamp(x[:, 3:4, :, :], min=0.0, max=1.0)
+
+def to_rgb(x):
+  # assume rgb premultiplied by alpha
+  rgb= x[:, :3, :, :]
+  a = to_alpha(x)
+  return 1.0-a+rgb
+
+# 3 72 72 
+def make_circle_masks(n, h, w):
+  x = torch.linspace(-1.0, 1.0, w)[None, None, :]
+  y = torch.linspace(-1.0, 1.0, h)[None, :, None]
+  center = torch.FloatTensor(2, n, 1, 1).uniform_(-0.5, 0.5)
+  r = torch.FloatTensor(n, 1, 1).uniform_(0.1, 0.4)
+  x, y = (x-center[0])/r, (y-center[1])/r
+  mask = (x*x+y*y < 1.0).type(torch.FloatTensor)
+  print('MASK - permute', mask)
+  return mask
+
+def generate_pool_figures(pool, step_i):
+  tiled_pool = tile2d(to_rgb(pool.x[:49]))
+  fade = np.linspace(1.0, 0.0, 72)
+  ones = np.ones(72) 
+  tiled_pool[:, :72] += (-tiled_pool[:, :72] + ones[None, :, None]) * fade[None, :, None] 
+  tiled_pool[:, -72:] += (-tiled_pool[:, -72:] + ones[None, :, None]) * fade[None, ::-1, None]
+  tiled_pool[:72, :] += (-tiled_pool[:72, :] + ones[:, None, None]) * fade[:, None, None]
+  tiled_pool[-72:, :] += (-tiled_pool[-72:, :] + ones[:, None, None]) * fade[::-1, None, None]
+  imwrite('train_log/%04d_pool.jpg'%step_i, tiled_pool)
+
+def plot_loss(loss_log):
+  pl.figure(figsize=(10, 4))
+  pl.title('Loss history (log10)')
+  pl.plot(np.log10(loss_log), '.', alpha=0.1)
+  pl.show()
