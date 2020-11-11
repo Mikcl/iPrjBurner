aec755cb38d7d1d896f61468a990b04915df18a5
Mikcl <mikesmikes400@gmail.com>
2020-11-11 21:44:43 +0000

    no grad after layer 2

--- src/model/cell_growing_ca.py
+++ src/model/cell_growing_ca.py
@@ -4,10 +4,15 @@ from torch import nn
 class CellGrowingCA(nn.Module):
     def __init__(self, channels, dim=128):
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(in_channels=channels*3,out_channels=dim, kernel_size=1),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=dim, out_channels=channels, kernel_size=1),
-        )
+        self.conv1 = nn.Conv2d(in_channels=channels*3,out_channels=dim, kernel_size=1)
+        self.act1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=dim, out_channels=channels, kernel_size=1)
+        with torch.no_grad():
+            self.conv2.weight.zero_()
+
+
     def forward(self, x):
-        return self.net(x)
+        x = self.conv1(x)
+        x = self.act1(x)
+        x = self.conv2(x)
+        return  x
